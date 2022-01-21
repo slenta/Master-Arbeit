40b72786acf56c8e6678ced7bdfa56c064883fe3
Simon Lentz <u301617@mistralpp1.hpc.dkrz.de>
2022-01-21 22:34:19 +0100

    training

--- training_batch_script.sh
+++ training_batch_script.sh
@@ -6,12 +6,12 @@
 #SBATCH -n 1
 #SBATCH --cpus-per-task=64
 #SBATCH --nodelist=mg206
-#SBATCH --time=10:00:00
-#SBATCH --mem=32G
+#SBATCH --time=12:00:00
+#SBATCH --mem=64G
 
 module load cuda/10.0.130
 module load singularity/3.6.1-gcc-9.1.0
 
 singularity exec --bind /work/uo1075/u301617/ --nv /work/uo1075/u301617/Master-Arbeit/pytorch_gpu_new.sif \
  python train.py \
- --mask_year '2001_2020' --im_year '2020'
+ --mask_year '2004_2020' --im_year 'r8_9' --save_part 'part_2'
